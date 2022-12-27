c43b02b8d5f0300afd555edeceed4d50afe6e753
Sasidharan <sasidharanrasse@gmail.com>
2022-12-27 19:04:33 +0530

    First copy

--- Project/src/main/java/org/example/Main.java
+++ Project/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.apache.log4j.Logger;
+import org.example.connection.SqlConnection;
 import org.example.execute.MysqlExecute;
 import org.example.execute.PostgreSqlExecute;
 
@@ -8,18 +9,74 @@ import java.sql.*;
 
 public class Main {
     static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) throws SQLException {
 
         logger.info("Connected to MySql Database");
 
-        System.out.println("\t\n____*** 1. EXECUTING MYSQL QUERIES ***___\n");
         MysqlExecute m = new MysqlExecute();
-        m.ExecuteMysqlQuery();
+        ResultSet resultset1 = m.ExecuteMysqlQuery();
+
+      /*  resultset1.next();{
+            System.out.println("RollNo :" + resultset1.getString(1));
+            System.out.println("Empcode :" + resultset1.getString(2));
+            System.out.println("Ename :" + resultset1.getString(3));
+            System.out.println("Skill :" + resultset1.getString(4));
+        }*/
+
 
         logger.info("Postgres - Connected");
 
-        System.out.println("\t\n____*** 2. EXECUTING POSTGRESQL QUERIES ***___\n");
         PostgreSqlExecute p = new PostgreSqlExecute();
-        p.ExecutePostgresQuery();
+        ResultSet resultset2 = p.ExecutePostgresQuery();
+
+        /* resultset2.next();
+        {
+            System.out.println("RollNo :" + resultset2.getString(1));
+            System.out.println("Empcode :" + resultset2.getString(2));
+            System.out.println("Ename :" + resultset2.getString(3));
+            System.out.println("Skill :" + resultset2.getString(4));
+        }*/
+
+        CompareResultset(resultset1, resultset2);
+
+
     }
+
+    public static void CompareResultset(ResultSet resultSet1, ResultSet resultSet2) throws SQLException {
+      /*  try{
+        while((resultSet1.next() && resultSet2.next())) {
+            String result1 = resultSet1.getString(1);
+            String result2 = resultSet2.getString(1);
+            if (result1.startsWith(result2)) {
+                System.out.println("Data Match");
+                System.out.println(result1.startsWith(result2));
+            } else {
+                throw new SQLException();
+
+            }
+        }
+            } catch (SQLException e) {
+                System.out.println("Data Mismatch");
+
+            }
+        }*/
+        try {
+            while (resultSet1.next()) {
+                String result1 = resultSet1.getString(1);
+                while (resultSet2.next()) {
+                    String result2 = resultSet2.getString(1);
+                    if (result1.startsWith(result2)) {
+                        System.out.println("Data Match");
+                        System.out.println(result1.startsWith(result2));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("MisMatch Data");
+        }
+    }
+  public void functions(){
+
+  }
 }
